0a181e97b1d06b0db2e840e8e1c24512a9b6431f
Andrey Utkin <spaun@vda-team.com>
2015-11-30 23:30:02 +0600

    Add a few more keybindings for multiple cursors mode

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -7,10 +7,14 @@
 (define-key region-bindings-mode-map "v" 'rectangle-mark-mode)
 (define-key region-bindings-mode-map "a" 'mc/mark-all-like-this)
 (define-key region-bindings-mode-map "p" 'mc/mark-previous-like-this)
+(define-key region-bindings-mode-map "P" 'mc/skip-to-previous-like-this)
+(define-key region-bindings-mode-map (kbd "M-p") 'mc/unmark-previous-like-this)
 (define-key region-bindings-mode-map "n" 'mc/mark-next-like-this)
-(define-key region-bindings-mode-map "n" 'mc/mark-next-like-this)
+(define-key region-bindings-mode-map "N" 'mc/skip-to-next-like-this)
+(define-key region-bindings-mode-map (kbd "M-n") 'mc/unmark-next-like-this)
 
 (global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
+(global-set-key (kbd "C-S-p") 'mc/mark-pop)
 
 (global-set-key (kbd "M-g c") 'avy-goto-char-2)
 (global-set-key (kbd "M-g w") 'avy-goto-word-1)
