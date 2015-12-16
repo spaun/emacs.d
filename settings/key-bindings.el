6928abae4ac08fa6969d39b14afb58bc35f6c4a9
Andrey Utkin <spaun@vda-team.com>
2015-12-16 18:10:28 +0600

    Add keybingdings for magit

--- settings/key-bindings.el
+++ settings/key-bindings.el
@@ -24,4 +24,7 @@
 
 (global-set-key (kbd "C-x f") 'recentf-open-files)
 
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
 (provide 'key-bindings)
