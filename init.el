9bed49e18f4a9ff9363b7cf67f1f95309140b57f
Andrey Utkin <spaun@vda-team.com>
2019-09-12 17:16:11 +0700

    Adjust lsp settings

--- init.el
+++ init.el
@@ -173,6 +173,7 @@
     (setq lsp-prefer-flymake nil
           lsp-ui-sideline-enable nil
           lsp-ui-doc-enable nil
+          lsp-file-watch-threshold 15000
           lsp-intelephense-storage-path
           (no-littering-expand-var-file-name "intelephense")))
   (use-package company-lsp
