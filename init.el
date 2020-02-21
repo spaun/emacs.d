6c69d5ea140dc9eef6442d9713ce5b703f33e10c
Andrey Utkin <spaun@vda-team.com>
2020-02-21 21:03:29 +0700

    Add redis to intelephense stubs list

--- init.el
+++ init.el
@@ -180,11 +180,13 @@
   (use-package lsp-ui
     :config
     (defvar lsp-intelephense-storage-path)
+    (defvar lsp-intelephense-stubs)
     (setq lsp-ui-sideline-enable nil
           lsp-ui-doc-enable nil
           lsp-file-watch-threshold 20000
           lsp-intelephense-storage-path
-          (no-littering-expand-var-file-name "intelephense")))
+          (no-littering-expand-var-file-name "intelephense")
+          lsp-intelephense-stubs ["apache" "bcmath" "bz2" "calendar" "com_dotnet" "Core" "ctype" "curl" "date" "dba" "dom" "enchant" "exif" "fileinfo" "filter" "fpm" "ftp" "gd" "hash" "iconv" "imap" "interbase" "intl" "json" "ldap" "libxml" "mbstring" "mcrypt" "meta" "mssql" "mysqli" "oci8" "odbc" "openssl" "pcntl" "pcre" "PDO" "pdo_ibm" "pdo_mysql" "pdo_pgsql" "pdo_sqlite" "pgsql" "Phar" "posix" "pspell" "readline" "recode" "Reflection" "regex" "session" "shmop" "SimpleXML" "snmp" "soap" "sockets" "sodium" "SPL" "sqlite3" "standard" "superglobals" "sybase" "sysvmsg" "sysvsem" "sysvshm" "tidy" "tokenizer" "wddx" "xml" "xmlreader" "xmlrpc" "xmlwriter" "Zend OPcache" "zip" "zlib" "redis"]))
   (use-package company-lsp
     :commands company-lsp))
 
