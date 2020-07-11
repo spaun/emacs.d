7c39e0dae67be0dc31733ffd2705a533a495773d
Andrey Utkin <spaun@vda-team.com>
2020-07-11 13:18:55 +0700

    Add one more eyebrowse workspace

--- init.el
+++ init.el
@@ -182,7 +182,8 @@
 (use-package eyebrowse
   :bind (("M-1" . eyebrowse-switch-to-window-config-1)
          ("M-2" . eyebrowse-switch-to-window-config-2)
-         ("M-3" . eyebrowse-switch-to-window-config-3))
+         ("M-3" . eyebrowse-switch-to-window-config-3)
+         ("M-4" . eyebrowse-switch-to-window-config-4))
   :config
   (setq
    eyebrowse-new-workspace t)
