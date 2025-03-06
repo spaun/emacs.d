4adec4795de3e8f8c68f3064687cee0a149d0f35
Andrey Utkin <spaun@vda-team.com>
2025-03-06 23:39:58 +0700

    Do not change default bytecode cache location

--- early-init.el
+++ early-init.el
@@ -3,9 +3,6 @@
 ;;; Commentary:
 
 ;;; Code:
-(when (boundp 'native-comp-eln-load-path)
-  (startup-redirect-eln-cache (expand-file-name "var/eln-cache/" user-emacs-directory)))
-
 (setenv "LSP_USE_PLISTS" "true")
 
 (setq
